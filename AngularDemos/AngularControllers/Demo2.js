7358034d63e9ef78325ddbf41efa0b319cf9ae1a
R-If <mujawarprince@gmail.com>
2017-02-08 20:17:01 +0530

    Files added demo2

---
+++ AngularDemos/AngularControllers/Demo2.js
@@ -0,0 +1,10 @@
+﻿
+var myApp = angular.module("M2", []);
+
+myApp.controller("C2", function ($scope) {
+    var country = {
+        name = Australia
+        flag =    
+        flagAlt = AustraliaFlag
+    }
+});
