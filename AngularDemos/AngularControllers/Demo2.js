795f7d99e17ff85f3a42fea188889eae76ff11fe
R-If <mujawarprince@gmail.com>
2017-02-08 20:29:27 +0530

    demo2 changes

--- AngularDemos/AngularControllers/Demo2.js
+++ AngularDemos/AngularControllers/Demo2.js
@@ -3,8 +3,7 @@ var myApp = angular.module("M2", []);
 
 myApp.controller("C2", function ($scope) {
     var country = {
-        name = Australia
-        flag =    
-        flagAlt = AustraliaFlag
+        name: "USA",
+        flag:"D:\Projects\AngularJSDemos\AngularDemos\AngularDemos\Content/AustraliaFlag.png"
     }
 });
