0891d358fa5546215e3bf4b8cf494b242c5a06bd
R-If <mujawarprince@gmail.com>
2017-02-09 14:56:27 +0530

    Demo2 Completed

--- AngularDemos/AngularControllers/Demo2.js
+++ AngularDemos/AngularControllers/Demo2.js
@@ -4,6 +4,7 @@ var myApp = angular.module("M2", []);
 myApp.controller("C2", function ($scope) {
     var country = {
         name: "USA",
-        flag:"D:\Projects\AngularJSDemos\AngularDemos\AngularDemos\Content/AustraliaFlag.png"
+        flag: "/Content/AustraliaFlag.png"
     }
+    $scope.country = country;
 });
