78db6ab5a1f69f5f15cf1a61b9102466ab247662
R-If <mujawarprince@gmail.com>
2017-02-09 20:12:07 +0530

    ng-repeat Example

---
+++ AngularDemos/AngularControllers/ngRepeat.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="D:\Projects\AngularJSDemos\AngularDemos\AngularDemos\Scripts/angular.min.js" />
+
+
+var myApp = angular.module("M3", []);
+
+myApp.controller("C3", function ($scope) {
+    var employee = [
+        { Firstname: "A", Lastname: "Mujawar", City: "Mumbai" },
+        { Firstname: "A", Lastname: "Mujawar", City: "Mumbai" },
+        { Firstname: "A", Lastname: "Mujawar", City: "Mumbai" },
+        { Firstname: "A", Lastname: "Mujawar", City: "Mumbai" },
+    ];
+
+    $scope.employee = employee;
+});
