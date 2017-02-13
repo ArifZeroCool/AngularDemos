26e35d38e6623b39a2bde2cc6f335534cbc22f08
R-If <mujawarprince@gmail.com>
2017-02-13 16:07:51 +0530

    Likes/Dislikes

---
+++ AngularDemos/AngularControllers/LikesDislikes.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="D:\Projects\AngularJSDemos\AngularDemos\AngularDemos\Scripts/angular.min.js" />
+
+var myApp = angular.module("LikesDislikes", [])
+.controller("LikesDislikesC", function ($scope) {
+    var technologies = [
+        { name: "C#", likes: 0, dislikes: 0 },
+        { name: "Python", likes: 0, dislikes: 0 },
+        { name: "Java", likes: 0, dislikes: 0 },
+        { name: "Angular", likes: 0, dislikes: 0 },
+    ];
+    $scope.technologies = technologies;
+
+    $scope.incrementLikes = function (technology) {
+        technology.likes++;
+    }
+    $scope.incrementDisLikes = function (technology) {
+        technology.dislikes++;
+    }
+    $scope.Reset = function (technology) {
+        technology.dislikes = 0;
+        technology.likes = 0;
+    }
+});
