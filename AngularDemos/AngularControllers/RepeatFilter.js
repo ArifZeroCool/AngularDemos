c3ff4b05614c07c65fec1036861f9b846615fca0
R-If <mujawarprince@gmail.com>
2017-02-16 19:49:03 +0530

    Filter and Sorting example

---
+++ AngularDemos/AngularControllers/RepeatFilter.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="D:\Projects\AngularJSDemos\AngularDemos\AngularDemos\Scripts/angular.min.js" />
+
+
+var myApp = angular.module("RFilter", [])
+.controller("FilterC", function ($scope) {
+    var employees = [
+        { name: "cdfdfdf", DOB: new Date("November 23,1995"), gender: "Male", salary: 55000.235 },
+        { name: "adfdf", DOB: new Date("November 23,1995"), gender: "Female", salary: 55000.456 },
+        { name: "rdfdf", DOB: new Date("November 23,1995"), gender: "Male", salary: 55000.278 },
+        { name: "bfgfg", DOB: new Date("November 23,1995"), gender: "Female", salary: 55000.256 },
+        { name: "hfgfgg", DOB: new Date("November 23,1995"), gender: "Male", salary: 55000.235 },
+    ];
+    $scope.employees = employees;
+    $scope.RowLimit = 3;
+    $scope.sortColumn = name;
+});
